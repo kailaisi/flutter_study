311dba9e2bfc0bdb6738c3fbdcc1fd174f7d83ae
kailaisii <541018378@qq.com>
2020-10-11 17:03:02 +0800

    侧边栏效果处理

--- lib/learn/statefull/pages/BottomBar.dart
+++ lib/learn/statefull/pages/BottomBar.dart
@@ -55,13 +55,36 @@ class _TabState extends State<TabWidget> {
               Row(
                 children: [
                   Expanded(
-                    child: DrawerHeader(
-                        child: CircleAvatar(
-                          backgroundImage: NetworkImage(
-                              "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2052053857,655414597&fm=26&gp=0.jpg"),
-                        ),
-                        decoration: BoxDecoration(color: Colors.green)),
-                  ),
+                    // 基于DrawerHeader实现
+                    // child: DrawerHeader(
+                    //     child: CircleAvatar(
+                    //       backgroundImage: NetworkImage(
+                    //           "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2052053857,655414597&fm=26&gp=0.jpg"),
+                    //     ),
+                    //     decoration: BoxDecoration(color: Colors.green)),
+                    child: UserAccountsDrawerHeader(
+                      accountName: Text("开了肯"),
+                      accountEmail: Text("541018378@qq.com"),
+                      currentAccountPicture: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2052053857,655414597&fm=26&gp=0.jpg"),
+                      ),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1602406606&di=85ca63bc366f4a1422db63ef62d2ec94&src=http://pic1.win4000.com/wallpaper/6/577dff88921e2.jpg"),
+                            fit: BoxFit.cover),
+                      ),
+                      otherAccountsPictures: [
+                        Image.network(
+                            "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1602406606&di=85ca63bc366f4a1422db63ef62d2ec94&src=http://pic1.win4000.com/wallpaper/6/577dff88921e2.jpg"),
+                        Image.network(
+                            "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1602406606&di=85ca63bc366f4a1422db63ef62d2ec94&src=http://pic1.win4000.com/wallpaper/6/577dff88921e2.jpg"),
+                        Image.network(
+                            "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1602406606&di=85ca63bc366f4a1422db63ef62d2ec94&src=http://pic1.win4000.com/wallpaper/6/577dff88921e2.jpg"),
+                      ],
+                    ),
+                  )
                 ],
               ),
               ListTile(
